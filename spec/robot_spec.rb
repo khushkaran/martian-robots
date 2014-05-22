629428df0937202cbdc5cf9322a6ed230665b0ba
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-22 15:38:26 +0100

    Added specs for robot

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 require 'robot'
 
 describe Robot do
+  let(:robot) { Robot.new("1 1 E") }
+	it "can be initialised with a position" do
+    expect(robot.position).to eq [1,1]
+  end
+
+  it "can be initialised with an orientation" do
+    expect(robot.position).to eq ["E"]
+  end
 end

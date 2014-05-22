b56bdf6888a104371e882a780f03d4bdaa16f6e0
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-22 15:22:47 +0100

    Mars can be initialised with a grid

--- lib/mars.rb
+++ lib/mars.rb
@@ -1,2 +1,18 @@
 class Mars
+  attr_reader :grid
+  def initialize(grid_size)
+    grid_size = grid_size.split(" ")
+    width = grid_size[0].to_i
+    height = grid_size[1].to_i
+    @grid = create_grid(width, height)
+  end
+
+  def create_grid(width, height)
+    (0..height).map{|row_number|
+      (0..width).map{|column_number|
+        [column_number, row_number]
+      }
+    }
+  end
 end
+
--- spec/mars_spec.rb
+++ spec/mars_spec.rb
@@ -2,5 +2,15 @@ require 'spec_helper'
 require 'mars'
 
 describe Mars do
-  
+  context "can be initialised with a size" do
+  	let(:mars) { Mars.new("5 3") }
+  	it "and has a height" do
+	  	expect(mars.grid.length).to eq 4
+  	end
+
+    it "and has a width" do
+      expect(mars.grid[0].length).to eq 6
+      p mars.grid
+    end
+  end
 end

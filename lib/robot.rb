d6b4beba5d36229ff2651d3dc026e7dd12871515
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-22 16:46:36 +0100

    Robot can change orientation

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,3 +1,5 @@
+DIRECTIONS = ["N", "E", "S", "W"]
+
 class Robot
   attr_reader :position
   attr_reader :orientation
@@ -7,6 +9,21 @@ class Robot
     x = location[0].to_i
     y = location[1].to_i
     @position = [x, y]
+  end
+
+  def follow_instructions(instructions)
+    instructions.chars.each{|instruction|
+      if instruction == "F"
+        raise NotYetImplemented
+      else
+        cur_dir = DIRECTIONS.index(@orientation)
+        if instruction == "R"
+          @orientation = DIRECTIONS[cur_dir+1]
+        else
+          @orientation = DIRECTIONS[cur_dir-1]
+        end
+      end
+    }
   end
 end
 
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -10,4 +10,11 @@ describe Robot do
   it "can be initialised with an orientation" do
     expect(robot.orientation).to eq "E"
   end
+
+  it "can change orientation" do
+    robot.follow_instructions("R")
+    expect(robot.orientation).to eq "S"
+    robot.follow_instructions("LL")
+    expect(robot.orientation).to eq "N"
+  end
 end

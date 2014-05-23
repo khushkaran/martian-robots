4e529bb57a0247de5fc40b7d0187e8771a238e24
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-23 12:41:23 +0100

    Added sessions to capture report

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require "sinatra"
 require_relative "lib/command_centre"
 
+enable :sessions
+set :session_secret, 'badgerbadgerbadgermushroommushroom'
+
 get '/' do
-  @report = "<-----------\nEnter some instructions"
+  @report = session[:report] ? session[:report] : "<-----------\nEnter some instructions"
   erb :index
 end
 
 post '/' do
+  comcen = CommandCentre.new(params[:instructions])
+  session[:report] = comcen.mars.earth_report
   redirect '/'
 end
